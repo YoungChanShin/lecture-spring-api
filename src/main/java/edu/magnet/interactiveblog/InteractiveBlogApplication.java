68c65970d0e361aa79e1aa211b40fd15609f7f74
Youngchan Shin <chan8149@naver.com>
2023-07-17 19:33:55 +0900

    first commit

---
+++ src/main/java/edu/magnet/interactiveblog/InteractiveBlogApplication.java
@@ -0,0 +1,13 @@
+package edu.magnet.interactiveblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/magnet/interactiveblog/InteractiveBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.magnet.interactiveblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
