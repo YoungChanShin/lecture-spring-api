35b4ad8161af4a038bd70be04a52dcfe35f8fdaa
Youngchan Shin <chan8149@naver.com>
2023-07-19 19:59:07 +0900

    validatie wrong data

--- src/main/java/edu/magnet/interactiveblog/events/EventController.java
+++ src/main/java/edu/magnet/interactiveblog/events/EventController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -21,17 +23,27 @@ public class EventController {
 
     private final ModelMapper modelMapper;
 
+    private final EventValidator eventValidator;
+
     @Autowired
-    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator) {
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto ) {
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        eventValidator.validate(eventDto, errors);
 //        Model Mapper
         Event event = modelMapper.map(eventDto, Event.class);
-
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
--- src/test/java/edu/magnet/interactiveblog/events/EventControllerTests.java
+++ src/test/java/edu/magnet/interactiveblog/events/EventControllerTests.java
@@ -38,10 +38,10 @@ public class EventControllerTests {
         EventDto event = EventDto.builder()
                 .name("spring")
                 .description("REST API")
-                .beginEnrollmentDateTime(LocalDateTime.of(2023,7,18,19,53))
+                .beginEnrollmentDateTime(LocalDateTime.of(2023,6,18,19,53))
                 .closeEnrollmentDateTime(LocalDateTime.of(2023,7,19,19,53))
-                .beginEventDateTime(LocalDateTime.of(2023,7,18,19,53))
-                .endEventDateTime(LocalDateTime.of(2023,7,18,19,53))
+                .beginEventDateTime(LocalDateTime.of(2023,8,18,19,53))
+                .endEventDateTime(LocalDateTime.of(2023,9,18,19,53))
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
@@ -93,5 +93,36 @@ public class EventControllerTests {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
         ;
+    }
+
+    @Test
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON )
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("spring")
+                .description("REST API")
+                .beginEnrollmentDateTime(LocalDateTime.of(2023,7,18,19,53))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023,7,19,19,53))
+                .beginEventDateTime(LocalDateTime.of(2023,7,18,19,53))
+                .endEventDateTime(LocalDateTime.of(2023,7,18,19,53))
+                .basePrice(1000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("상공회의소")
+                .build();
+
+        this.mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON )
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
     }
 }
